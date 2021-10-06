f5d686fb57e9cf893dda922731f14c106ec3566e
Lami <Halam.Lee.Lami@gmail.com>
2021-10-06 15:14:48 +0900

    로딩 구현

---
+++ loading.js
@@ -0,0 +1,7 @@
+const loading = document.querySelector(".loading");
+const main = document.querySelector(".main");
+
+setTimeout(function () {
+  loading.style.display = "none";
+  main.style.display = "block";
+}, 5000);
