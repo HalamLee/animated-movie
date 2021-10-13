0f0580eeb4cad604205b8a9198b1a90c02faae84
Lami <Halam.Lee.Lami@gmail.com>
2021-10-14 05:45:10 +0900

    로딩화면 기능 삭제

--- loading.js
+++
@@ -1,9 +0,0 @@
-const loading = document.querySelector(".loading");
-const scene = document.querySelector(".scene");
-const mainScene = document.querySelector(".main-scene");
-
-setTimeout(function () {
-  loading.style.display = "none";
-  scene.style.height = "100%";
-  mainScene.style.display = "block";
-}, 5000);
